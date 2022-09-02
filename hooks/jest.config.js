2cc1985487c894643dff068fb94f4b7628f6a1b2
Nikolay Smekalin <nnsmekalin@domclick.ru>
2022-09-02 03:22:24 +0300

    init hooks project

---
+++ hooks/jest.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+module.exports = {
+  bail: 1,
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  rootDir: 'src',
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '.(js|jsx|ts|tsx)$': 'ts-jest'
+  },
+  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]]
+};
---
+++ hooks/src/useDebounce/use-debounce.ts
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from 'react';
+
+function useDebounce<A extends any[]>(func: (...args: A) => void, delay: number, cleanUp = false) {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+
+  function clearTimer() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = undefined;
+    }
+  }
+
+  useEffect(() => (cleanUp ? clearTimer : undefined), [cleanUp]);
+
+  return (...args: A) => {
+    clearTimer();
+    timeoutRef.current = setTimeout(() => func(...args), delay);
+  };
+}
+
+export default useDebounce;
