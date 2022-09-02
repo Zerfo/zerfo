fbb47463c893bf157fd01a396502a53d3c01bae9
Nikolay Smekalin <nnsmekalin@domclick.ru>
2022-09-02 11:09:02 +0300

    update useWidget tests

---
+++ hooks/src/useDebounce/use-debounce.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { rendererHook } from '@utils/render-hook';
+
+import { useDebounce } from '@src/useDebounce';
+
+jest.useFakeTimers('modern');
+
+describe('useDebounce', () => {
+  it.todo('put initialized value in first render')
+});
