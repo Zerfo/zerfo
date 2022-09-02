b93831026361b1021eaa4c9486561005eee6c1b8
Nikolay Smekalin <nnsmekalin@domclick.ru>
2022-09-02 19:51:25 +0300

    add useInterval

---
+++ hooks/src/useInterval/use-interval.test.tsx
@@ -0,0 +1,5 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useInterval } from '@src/useInterval';
+
+describe('useInterval', () => {});
---
+++ hooks/src/useInterval/use-interval.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+export function useInterval(func: Function, delay?: number | null) {
+  const savedCallback = useRef<Function>();
+
+  useEffect(() => {
+    savedCallback.current = func;
+  }, [delay, func]);
+
+  useEffect(() => {
+    if (delay !== null) {
+      const interval = setInterval(() => savedCallback.current(), delay || 0);
+      return () => clearInterval(interval);
+    }
+
+    return undefined;
+  }, [delay]);
+}
+
+export default useInterval;
